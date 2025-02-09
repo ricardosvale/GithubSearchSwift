3140db97be416ab21c86689d0ba2b6146dcdd9be
ricardosvale <ricardost3@gmail.com>
2025-02-08 23:37:35 -0300

    feat: ProfileView

--- GithubSearch/Profile/ProfileView.swift
+++ GithubSearch/Profile/ProfileView.swift
@@ -8,24 +8,95 @@
 import SwiftUI
 
 struct ProfileView: View {
-    var username: String
+    @Environment(\.dismiss) private var dismiss
+    @StateObject private var profileViewModel: ProfileViewModel
+    
+    init(username: String) {
+        _profileViewModel = StateObject(wrappedValue: ProfileViewModel(username: username))
+    }
+    
     var body: some View {
-        VStack {
-            AsyncImage(url:URL(string: "https://avatars.githubusercontent.com/u/169118123?v=4"))
-                .clipShape(Circle())
-            Text("Name")
-            
-            ScrollView{
+        NavigationStack {
+            VStack {
                 VStack {
-                    Text("Name Repositorie")
-                    Text("linguage Repositorie")
+                    if let imageUrl = URL(string: profileViewModel.imageUrl) {
+                        AsyncImage(url: imageUrl) { phase in
+                            switch phase {
+                            case .empty:
+                                ProgressView()
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 150, height: 150)
+                                    .clipShape(Circle())
+                            case .failure:
+                                Image(systemName: "person.crop.circle.badge.exclamationmark")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 150, height: 150)
+                                    .clipShape(Circle())
+                                    .foregroundStyle(.gray)
+                            @unknown default:
+                                EmptyView()
+                            }
+                        }
+                       
+                        Text(profileViewModel.username)
+                            .font(.title3)
+                            .padding(.top, 5)
+                    }
+                    
+                    if let errorMessage = profileViewModel.errorMessage {
+                        Text(errorMessage)
+                            .foregroundStyle(.red)
+                    }
+                }
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .center)
+                .background(Color.gray.opacity(0.1))
+                .zIndex(1)
+                
+                VStack {
+                    ScrollView {
+                        LazyVStack(alignment: .leading) {
+                            ForEach(profileViewModel.repositories, id: \.name) { repository in
+                                VStack(alignment: .leading) {
+                                    Text(repository.name)
+                                        .font(.headline)
+                                        .foregroundStyle(.black.opacity(0.6))
+                                    Text(repository.language ?? "No language")
+                                        .font(.subheadline)
+                                        .foregroundStyle(.gray)
+                                }
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 15)
+                                
+                                
+                                Divider()
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                             }
+                        }
+                    }
                 }
             }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                            Text("Back")
+                        }
+                    }
+                }
+            }
+           .navigationBarTitleDisplayMode(.inline)
         }
-      
     }
 }
 
 #Preview {
-    ProfileView(username: "")
+    ProfileView(username: "ricardosvale")
 }
